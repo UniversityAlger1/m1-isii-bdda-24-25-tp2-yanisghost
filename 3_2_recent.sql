0fc24fdb316bccdd73665b08043642024253af48
yanisghost <lammalimohamedyanis@gmail.com>
2025-05-03 01:52:45 +0100

    Update 3_2_recent.sql

--- 3_2_recent.sql
+++ 3_2_recent.sql
@@ -1,1 +1,8 @@
-
+-- Display the name (NomLab) of the most recent created Laboratoire
+SELECT NomLab
+FROM Laboratoire
+WHERE Datcreation = (
+    SELECT MAX(Datcreation)
+    FROM Laboratoire
+);
+GO
