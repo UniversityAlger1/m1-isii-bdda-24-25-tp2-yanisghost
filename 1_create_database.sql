f23c75677eb1aaf88b757ca9d7f28771bdabbc40
yanisghost <lammalimohamedyanis@gmail.com>
2025-05-03 01:53:53 +0100

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,5 +1,5 @@
 -- Create LabDatabase Database
-
+CREATE DATABASE LabDatabase;
 GO
 
 -- Use LabDatabase Database
@@ -7,9 +7,20 @@ USE LabDatabase;
 GO 
 
 -- Create Laboratoire Table
-
+CREATE TABLE Laboratoire (
+    CodeLab VARCHAR(4) PRIMARY KEY,
+    NomLab VARCHAR(100),
+    Datcreation DATE,
+    DatFin DATE,
+    Siteweb NVARCHAR(255)
+);
+GO
 
 -- Create Chercheur Table 
-
-
+CREATE TABLE Chercheur (
+    NumCh INT PRIMARY KEY,
+    NomCh VARCHAR(30),
+    CodeLab VARCHAR(4),
+    FOREIGN KEY (CodeLab) REFERENCES Laboratoire(CodeLab)
+);
 GO
